dc8f02d5c580b602545a5ad26c3fa1c9d5d37afc
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-04-21 23:55:27 -0300

    feat: hall

--- js/rough.js
+++ js/rough.js
@@ -1,25 +1,61 @@
 import { annotate } from 'https://unpkg.com/rough-notation?module';
 
 const n1 = document.querySelector('#tittleSB');
-const a1 = annotate(n1, { type: 'highlight', color: '#631f63', padding: 10 });
+const a1 = annotate(n1, { type: 'highlight', color: '#8A2BE2', padding: 10 });
 
 const n2 = document.querySelector('#draw');
-const a2 = annotate(n2, { type: 'box', color: '#631f63', padding: 5 });
+const a2 = annotate(n2, { type: 'box', color: '#8A2BE2', padding: 5 });
 
 const n3 = document.querySelector('#draw2');
-const a3 = annotate(n3, { type: 'underline', color: '#631f63', padding: 0 });
+const a3 = annotate(n3, { type: 'underline', color: '#8A2BE2', padding: 0 });
 
 const n4 = document.querySelector('#draw3');
-const a4 = annotate(n4, { type: 'underline', color: '#631f63', padding: 0 });
+const a4 = annotate(n4, { type: 'underline', color: '#8A2BE2', padding: 0 });
 
 const n5 = document.querySelector('#cardsRough1');
-const a5 = annotate(n5, { type: 'box', color: '#631f53', padding: 0 });
+const a5 = annotate(n5, { type: 'box', color: '#8A2BE2', padding: 0 });
 
 const n6 = document.querySelector('#cardsRough2');
-const a6 = annotate(n6, { type: 'box', color: '#631f53', padding: 0 });
+const a6 = annotate(n6, { type: 'box', color: '#8A2BE2', padding: 0 });
 
 const n7 = document.querySelector('#cardsRough3');
-const a7 = annotate(n7, { type: 'box', color: '#631f53', padding: 0 });
+const a7 = annotate(n7, { type: 'box', color: '#8A2BE2', padding: 0 });
+
+const n8 = document.querySelector('#number1');
+const a8 = annotate(n8, { type: 'box', color: '#353535', padding: 5 });
+
+const n9 = document.querySelector('#number2');
+const a9 = annotate(n9, { type: 'box', color: '#353535', padding: 5 });
+
+const n10 = document.querySelector('#number3');
+const a10 = annotate(n10, { type: 'box', color: '#353535', padding: 5 });
+
+const n11 = document.querySelector('#number4');
+const a11 = annotate(n11, { type: 'box', color: '#353535', padding: 5 });
+
+const n12 = document.querySelector('#number5');
+const a12 = annotate(n12, { type: 'box', color: '#353535', padding: 5 });
+
+const n13 = document.querySelector('#number6');
+const a13 = annotate(n13, { type: 'box', color: '#353535', padding: 5 });
+
+const n14 = document.querySelector('#number7');
+const a14 = annotate(n14, { type: 'box', color: '#353535', padding: 5 });
+
+const n15 = document.querySelector('#number8');
+const a15 = annotate(n15, { type: 'box', color: '#353535', padding: 5 });
+
+const n16 = document.querySelector('#number9');
+const a16 = annotate(n16, { type: 'box', color: '#353535', padding: 5 });
+
+const n17 = document.querySelector('#img-boxe1');
+const a17 = annotate(n17, { type: 'box', color: '#fff', padding: 0 });
+
+const n18 = document.querySelector('#img-boxe2');
+const a18 = annotate(n18, { type: 'box', color: '#fff', padding: 0 });
+
+const n19 = document.querySelector('#img-boxe3');
+const a19 = annotate(n19, { type: 'box', color: '#fff', padding: 0 });
 
 a1.show();
 a2.show();
@@ -28,3 +64,15 @@ a4.show();
 a5.show();
 a6.show();
 a7.show();
+a8.show();
+a9.show();
+a10.show();
+a11.show();
+a12.show();
+a13.show();
+a14.show();
+a15.show();
+a16.show();
+a17.show();
+a18.show();
+a19.show();
