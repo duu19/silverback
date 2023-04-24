e6d8cbd8d7eafc333ae45b2a9be0795e1abde32c
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-04-23 21:42:59 -0300

    fix: style - cores

--- js/rough.js
+++ js/rough.js
@@ -1,25 +1,25 @@
 import { annotate } from 'https://unpkg.com/rough-notation?module';
 
 const n1 = document.querySelector('#tittleSB');
-const a1 = annotate(n1, { type: 'highlight', color: '#8A2BE2', padding: 10 });
+const a1 = annotate(n1, { type: 'highlight', color: '#612E6C', padding: 10 });
 
 const n2 = document.querySelector('#draw');
-const a2 = annotate(n2, { type: 'box', color: '#8A2BE2', padding: 5 });
+const a2 = annotate(n2, { type: 'box', color: '#612E6C', padding: 5 });
 
 const n3 = document.querySelector('#draw2');
-const a3 = annotate(n3, { type: 'underline', color: '#8A2BE2', padding: 0 });
+const a3 = annotate(n3, { type: 'underline', color: '#612E6C', padding: 0 });
 
 const n4 = document.querySelector('#draw3');
-const a4 = annotate(n4, { type: 'underline', color: '#8A2BE2', padding: 0 });
+const a4 = annotate(n4, { type: 'underline', color: '#612E6C', padding: 0 });
 
 const n5 = document.querySelector('#cardsRough1');
-const a5 = annotate(n5, { type: 'box', color: '#8A2BE2', padding: 0 });
+const a5 = annotate(n5, { type: 'box', color: '#111', padding: 0 });
 
 const n6 = document.querySelector('#cardsRough2');
-const a6 = annotate(n6, { type: 'box', color: '#8A2BE2', padding: 0 });
+const a6 = annotate(n6, { type: 'box', color: '#111', padding: 0 });
 
 const n7 = document.querySelector('#cardsRough3');
-const a7 = annotate(n7, { type: 'box', color: '#8A2BE2', padding: 0 });
+const a7 = annotate(n7, { type: 'box', color: '#111', padding: 0 });
 
 const n8 = document.querySelector('#number1');
 const a8 = annotate(n8, { type: 'box', color: '#353535', padding: 5 });
