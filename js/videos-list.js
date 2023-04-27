f7fdd6187ae0fc6301cf6b2ac1ad22ce3ff21d50
Eduardo Henrique <duardohenriqe19@gmail.com>
2023-04-27 16:31:24 -0300

    feat: videos highlights

---
+++ js/videos-list.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        src: "../videos/tank.mp4",
+        title: "tank",
+    },
+    {
+        src: "../videos/pacman.mp4",
+        title: "pacman",
+    },
+    {
+        src: "../videos/wilder.mp4",
+        title: "bomber",
+    },
+    {
+        src: "../videos/ruiz.mp4",
+        title: "ruiz",
+    },
+    {
+        src: "../videos/jermall.mp4",
+        title: "hit man",
+    },
+    {
+        src: "../videos/cruz.mp4",
+        title: "pitbull",
+    },
+];
